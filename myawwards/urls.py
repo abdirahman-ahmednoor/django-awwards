9752ad701014a10c2b5e616013b195b4fed2f905
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-09-21 10:28:58 +0300

    remove unnnecessary imports

--- myawwards/urls.py
+++ myawwards/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import url
-from django.urls import re_path,path,include
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views 
 
